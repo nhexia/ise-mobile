6630c1e4086cfbd9c96a65436fd7f7d3a5372ed3
nhexia22 <reinhardn@ionline.com.au>
2018-06-18 20:13:13 +1000

    upload

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,271 +1,250 @@
 angular.module('app.controllers', [])
 
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout,$http,Users,$localStorage) {
+        .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $http, Users, $localStorage) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+            // Form data for the login modal
+            $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+            // Create the login modal that we will use later
+            $ionicModal.fromTemplateUrl('templates/login.html', {
+                scope: $scope
+            }).then(function (modal) {
+                $scope.modal = modal;
+            });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+            // Triggered in the login modal to close it
+            $scope.closeLogin = function () {
+                $scope.modal.hide();
+            };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+            // Open the login modal
+            $scope.login = function () {
+                $scope.modal.show();
+            };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+            // Perform the login action when the user submits the login form
+            $scope.doLogin = function () {
+                console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-  $scope.name = $localStorage.userData.complete_name;
-  console.log(name);
-	
-})
+                // Simulate a login delay. Remove this and replace with your login
+                // code if using a login system
+                $timeout(function () {
+                    $scope.closeLogin();
+                }, 1000);
+            };
+            $scope.name = $localStorage.userData.complete_name;
+            console.log(name);
 
-.controller('ManifestCtrl', function($ionicLoading,$scope,$location,$localStorage,$state,Book) {
-	$scope.results = [];
-	$ionicLoading.show({
-      template: 'loading manifest...',
-     
-    });
-    
-	Book.getAll().then(function (result) {
-						
-			$scope.results = [];
+        })
 
-			$scope.results  = angular.fromJson(result); 
-		$ionicLoading.hide();
-	});		
-	
-	
-	
-	var a = 1;
-	$scope.saveAll = function(){
-		
-		angular.forEach($scope.results  , function (value, key) {
-			//console.log(value)
-                     Book.update(value).then(function(result){
-						   console.log(result)
-					   });   
-				 
-			if($scope.results.length == a){
-				
-				$state.go($state.current, {}, {reload: true})
-			}
-			a++;
-			
-	
-		});
-		
-		
-	}
-	
-	
-	$scope.copyData = function(data){
-		$ionicLoading.show({
-				  template: 'Loading...',
-			});
-			Book.get(data).then(function(result){
-				console.log(result);
-				document.getElementById(data+"-a").value = result.book_a;
-				document.getElementById(data+"-k").value = result.book_k;
+        .controller('ManifestCtrl', function ($ionicLoading, $scope, $location, $localStorage, $state, Book) {
+            $scope.results = [];
+            $ionicLoading.show({
+                template: 'loading manifest...',
 
-				document.getElementById(data+"-foc").value = result.book_foc;
-				document.getElementById(data+"-inf").value = result.book_inf;
-				document.getElementById(data+"-tg").value = result.book_tg;
-				document.getElementById(data+"-e").value = result.book_e;
-				document.getElementById(data+"-arrival").value = result.book_arrival;
-				document.getElementById(data+"-time").value = result.book_time;
-				$ionicLoading.hide();
-			})
-		}
-    
-	
+            });
 
-})
-.controller('SettingCtrl', function($scope,$location,$ionicLoading,$ionicPopup,$localStorage,$state,Book) {
-	$scope.serverUrl = "";
-	
-	if($localStorage.serverUrl == null){
-	
-	   
-		$scope.$storage = "localhost";
-		
-	}else{
-			$scope.$storage = $localStorage;
-		
-	}
-	
-	$scope.saveServerUrl = function(){
-		
-			delete $localStorage.serverUrl;
-			 $localStorage.serverUrl = $scope.serverUrl;
-		
-		console.log("submitted : "+$scope.serverUrl);
-		var confirmPopup = $ionicPopup.confirm({
-				 scope:$scope,
-				 title: 'Success',
-				 template: 'Server URL saved',
-				 buttons: [
-			 
-				   {
-					 text: '<b>OK</b>',
-					 type: 'button-balanced',
-					
-				   }
-				   ]
-			 });
-			 
-		
-	}
-	
-	
-	$scope.syncBook = function(){
-			$ionicLoading.show({
-				  template: 'Updating data...',
-			});
-			
-			
-		$scope.result = Book.updateSync();
-		
-		if($scope.result){
-			$ionicLoading.hide();
-			 var confirmPopup = $ionicPopup.confirm({
-				 scope:$scope,
-				 title: 'Details',
-				 template: ' Updating data sucessfuly!',
-				 buttons: [
-			 
-				   {
-					 text: '<b>Done</b>',
-					 type: 'button-balanced',
-					
-				   }
-				   ]
-			 });
-			
-		}else{
-			var confirmPopup = $ionicPopup.confirm({
-				 scope:$scope,
-				 title: 'Error',
-				 template: 'Cant connect to server.',
-				 buttons: [
-			 
-				   {
-					 text: '<b>Done</b>',
-					 type: 'button-balanced',
-					
-				   }
-				   ]
-			 });
-			$ionicLoading.hide();
-		}
-		
-	
-	}
-	$scope.logout = function(){
-		 var confirmPopup = $ionicPopup.confirm({
-        title: 'Logout',
-        template: 'Are you sure you want to logout?',
-        cancelText: 'No',
-        okText: 'Yes'
-		}).then(function(res) {
-			if (res) {
-				$localStorage.userData = null;
-				  
-				  $state.go('login');
-			}
-		});
-		
-	
-	}
-})
-.controller('LoginCtrl', function($scope,Users, $location,$http,$localStorage,$ionicLoading,DBA) {
-	$ionicLoading.show({
-      template: 'Loading...',
-     
-    }).then(function(){
-       console.log("Checking user data...");
-    });
-	
-	$http.get('//localhost/?getUser=1').success(function (data, status) {
-	   $scope.users = data;
-		  angular.forEach($scope.users,  function (value, key) {
+            Book.getAll().then(function (result) {
+
+                $scope.results = [];
+
+                $scope.results = angular.fromJson(result);
+                $ionicLoading.hide();
+            });
 
 
-				Users.removeALL().then(function (result) {
-					Users.add(value).then(function (result) {
-						
-						console.log(result)
-						$ionicLoading.hide()
-					});
+
+            var a = 1;
+            $scope.saveAll = function () {
+
+                angular.forEach($scope.results, function (value, key) {
+                    //console.log(value)
+                    Book.update(value).then(function (result) {
+                        console.log(result)
+                    });
+
+                    if ($scope.results.length == a) {
+
+                        $state.go($state.current, {}, {reload: true})
+                    }
+                    a++;
+
+
                 });
-				
-        
-		})
-		    
-     }).error(function (error) {
-        console.log(error);
-		  $ionicLoading.hide()
-    });
-	
-	if($localStorage.userData){
-			$location.path('/app/manifest');
-	}
-	
-	$scope.loginData = {username:'admin',password:'123456'};
-	$scope.error = {};
-	
-	
-	$scope.loginMe = function() {
-		
-	
-		var parameters = [$scope.loginData.username,$scope.loginData.password];
-	
-		Users.login($scope.loginData).then(function (result) {
-			//console.log(result);	
-				if(result != false){
-					$scope.error[0]="";
-					$scope.error[1]="";
-					
-					console.log("result.ID="+result.complete_name)
-				
-					$localStorage.userData = result;
-					if($localStorage.serverUrl == null){
 
-						 $localStorage.serverUrl = "//localhost";
-						
-					}
-					$location.path('/app/manifest');
-				}else{
-					$scope.error[0]="Login Failed";
-					$scope.error[1]="Invalid Username or Password";
-				}
-	
-		});
-	}
-});
+
+            }
+
+
+            $scope.copyData = function (data) {
+                $ionicLoading.show({
+                    template: 'Loading...',
+                });
+                Book.get(data).then(function (result) {
+                    console.log(result);
+                    document.getElementById(data + "-a").value = result.book_a;
+                    document.getElementById(data + "-k").value = result.book_k;
+
+                    document.getElementById(data + "-foc").value = result.book_foc;
+                    document.getElementById(data + "-inf").value = result.book_inf;
+                    document.getElementById(data + "-tg").value = result.book_tg;
+                    document.getElementById(data + "-e").value = result.book_e;
+                    document.getElementById(data + "-arrival").value = result.book_arrival;
+                    document.getElementById(data + "-time").value = result.book_time;
+                    $ionicLoading.hide();
+                })
+            }
+
+
+
+        })
+        .controller('SettingCtrl', function ($scope, $location, $ionicLoading, $ionicPopup, $localStorage, $state, Book) {
+            $scope.serverUrl = "";
+
+            if ($localStorage.serverUrl == null) {
+
+
+                $scope.$storage = "sandbox.firstnet.com";
+
+            } else {
+                $scope.$storage = $localStorage;
+
+            }
+
+           
+
+
+            $scope.syncBook = function () {
+                $ionicLoading.show({
+                    template: 'Updating data...',
+                });
+
+
+                $scope.result = Book.updateSync();
+
+                if ($scope.result) {
+                    $ionicLoading.hide();
+                    var confirmPopup = $ionicPopup.confirm({
+                        scope: $scope,
+                        title: 'Details',
+                        template: ' Updating data sucessfuly!',
+                        buttons: [
+
+                            {
+                                text: '<b>Done</b>',
+                                type: 'button-balanced',
+
+                            }
+                        ]
+                    });
+
+                } else {
+                    var confirmPopup = $ionicPopup.confirm({
+                        scope: $scope,
+                        title: 'Error',
+                        template: 'Cant connect to server.',
+                        buttons: [
+
+                            {
+                                text: '<b>Done</b>',
+                                type: 'button-balanced',
+
+                            }
+                        ]
+                    });
+                    $ionicLoading.hide();
+                }
+
+
+            }
+            $scope.logout = function () {
+                var confirmPopup = $ionicPopup.confirm({
+                    title: 'Logout',
+                    template: 'Are you sure you want to logout?',
+                    cancelText: 'No',
+                    okText: 'Yes'
+                }).then(function (res) {
+                    if (res) {
+                        $localStorage.userData = null;
+
+                        $state.go('login');
+                    }
+                });
+
+
+            }
+        })
+        .controller('LoginCtrl', function ($scope, Users, $location, $http, $localStorage, $ionicLoading, DBA) {
+           $scope.loginData = {username: 'admin', password: '123456',serverUrl :"sandbox.firstnet.com"};
+            $ionicLoading.show({
+                template: 'Loading...',
+
+            }).then(function () {
+                console.log("Checking user data...");
+            });
+            
+            $http.get('//'+ $scope.loginData.serverUrl+'/?getUser=1').success(function (data, status) {
+                $scope.users = data;
+              
+                angular.forEach($scope.users, function (value, key) {
+                    
+                    
+                
+                
+                    Users.removeALL().then(function (result) {
+                          
+                        Users.add(value).then(function (result) {
+                            
+                            console.log(result)
+                            $ionicLoading.hide()
+                        });
+                    });
+
+
+                })
+
+            }).error(function (error) {
+                console.log(error);
+                $ionicLoading.hide()
+            });
+
+            if ($localStorage.userData) {
+                $location.path('/app/manifest');
+            }
+
+         
+            $scope.error = {};
+
+
+            $scope.loginMe = function () {
+
+
+                var parameters = [$scope.loginData.username, $scope.loginData.password];
+
+                Users.login($scope.loginData).then(function (result) {
+                    //console.log(result);	
+                    if (result != false) {
+                        $scope.error[0] = "";
+                        $scope.error[1] = "";
+                        result.serverUrl = $scope.loginData.serverUrl;
+
+                        $localStorage.userData = result;
+                       
+                        $location.path('/app/manifest');
+                    } else {
+                        $scope.error[0] = "Login Failed";
+                        $scope.error[1] = "Invalid Username or Password";
+                    }
+
+                });
+            }
+        });
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('app.services', [])
-			.factory('DBA', function ($cordovaSQLite, $q, $ionicPlatform) {
+        .factory('DBA', function ($cordovaSQLite, $q, $ionicPlatform) {
             var self = this;
 
             // Handle query's and potential errors
@@ -19,7 +19,7 @@ angular.module('app.services', [])
                 });
                 return q.promise;
             }
-			
+
 
             // Proces a result set
             self.getAll = function (result) {
@@ -40,7 +40,7 @@ angular.module('app.services', [])
 
             return self;
         }).
-		factory('Users', function ($cordovaSQLite, DBA, $filter, $http) {
+        factory('Users', function ($cordovaSQLite, DBA, $filter, $http) {
             var self = this;
 
             self.all = function () {
@@ -59,113 +59,117 @@ angular.module('app.services', [])
             }
 
             self.add = function (data) {
-				//console.log("data = "+data.ID,data.username,data.password,data.complete_name,data.status,data.last_sync);
-                var parameters = [data.ID,data.username,data.password,data.complete_name,data.status,data.last_sync];
+           
+                
+                
+                console.log(data);
+                var parameters = data;
                 return DBA.query("INSERT INTO user (ID,username ,password ,complete_name ,status ,last_sync )\n\
                                                          VALUES (?,?,?,?,?,?)", parameters);
             }
 
-            
-			self.removeALL = function () {
+
+            self.removeALL = function () {
                 var parameters = [];
                 return DBA.query("DELETE FROM user", parameters);
             }
-			
-			self.login = function (data) {
-				
-                var parameters = [data.username,data.password];
-				
-             
-				 return DBA.query("SELECT * FROM user WHERE username= (?) and password = (?)", parameters)
+
+            self.login = function (data) {
+
+                var parameters = [data.username, data.password];
+
+
+                return DBA.query("SELECT * FROM user WHERE username= (?) and password = (?)", parameters)
                         .then(function (result) {
-							
-							console.log(result);
-							var count = result.rows.length;
-							
-							if(count != 0){
-								return  result.rows[0];
-							}else{
-								
-								return false;
-							}
+
+                            console.log(result);
+                            var count = result.rows.length;
+
+                            if (count != 0) {
+                                return  result.rows[0];
+                            } else {
+
+                                return false;
+                            }
                         });
             }
 
 
-            
+
             return self;
         })
-		.factory('Book', function (DBA, $http,$rootScope,$localStorage,$ionicLoading) {
+        .factory('Book', function (DBA, $http, $rootScope, $localStorage, $ionicLoading) {
             var self = this;
-			var userId = $localStorage.userData.ID;
+            var userId = $localStorage.userData.ID;
+        
             self.updateSync = function () {
-				
-				
-				$http.get('//localhost/?getBooking='+userId).success(function (data) {
-	
-					$rootScope.book = data;
-					self.removeALL();
-					 angular.forEach(data,  function (value, key) {
-							//console.log(value);
-							self.add(value)
-						})
-				
-					return true;
-						
-				 }).error(function (error) {
-					return false;
-					
-				});
-				return true;
-				
+
+               var userServer = $localStorage.userData.serverUrl;
+           console.log($localStorage.userData);
+                $http.get('//'+userServer+'/?getBooking=' + userId).success(function (data) {
+
+                    $rootScope.book = data;
+                    self.removeALL();
+                    angular.forEach(data, function (value, key) {
+                        //console.log(value);
+                        self.add(value)
+                    })
+
+                    return true;
+
+                }).error(function (error) {
+                    return false;
+
+                });
+                return true;
+
             }
-			 self.getAll = function () {
-				 var userId = $localStorage.userData.ID;
-				 var param = [userId];
-				 var items = [];
-                return DBA.query("SELECT * FROM book WHERE user = "+userId+"")
+            self.getAll = function () {
+                var userId = $localStorage.userData.ID;
+                var param = [userId];
+                var items = [];
+                return DBA.query("SELECT * FROM book WHERE user = " + userId + "")
                         .then(function (result) {
-                          if(result.rows.length > 0){
-							var itemsColl = [];
-								for (var i = 0; i < result.rows.length; i++) {
-									itemsColl[i] = result.rows.item(i);
+                            if (result.rows.length > 0) {
+                                var itemsColl = [];
+                                for (var i = 0; i < result.rows.length; i++) {
+                                    itemsColl[i] = result.rows.item(i);
 
-								};
+                                }
+                                ;
 
-									items = JSON.stringify(itemsColl);
-									
-									return items;
-							}
+                                items = JSON.stringify(itemsColl);
+
+                                return items;
+                            }
                         });
             }
 
-			self.add = function (data) {
-				
-                var parameters = [data.ID,data.location,data.cv_number,data.account_name,data.book_a,data.book_k,
-				data.book_foc,data.book_inf,data.book_tg,data.book_e,data.book_arrival,data.book_time,
-				data.actual_a,data.actual_k,data.actual_foc,data.actual_inf,data.actual_tg,data.actual_e,
-				data.actual_arrival,data.actual_time,data.resort_hotel,data.unit,data.unit_atd,data.driver,
-				data.coordinator,data.sales_handle,data.agency,data.remarks,data.user,data.status]
-				
-				
-                return DBA.query("INSERT INTO book (ID, location, cv_number, account_name, book_a, book_k, book_foc, book_inf, book_tg, book_e, book_arrival, book_time, actual_a, actual_k, actual_foc, actual_inf, actual_tg, actual_e, actual_arrival, actual_time, resort_hotel, unit, unit_atd, driver, coordinator, sales_handle, agency, remarks, user,status )\n\
-                                                         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", parameters);
+            self.add = function (data) {
+
+                var parameters = data;
+                console.log(data);
+
+                return DBA.query("INSERT INTO book (ID, location, cv_number, account_name, book_a, book_k, book_foc, book_inf, book_tg, book_e,\n\
+                                                     book_arrival, book_time, actual_a, actual_k, actual_foc, actual_inf, actual_tg, actual_e, actual_arrival, actual_time,\n\
+                                                     resort_hotel, unit, unit_atd, driver, coordinator, sales_handle, agency, remarks, user )\n\
+                                                         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", parameters);
             }
-			self.removeALL = function () {
+            self.removeALL = function () {
                 var parameters = [];
                 return DBA.query("DELETE FROM book", parameters);
             }
-			
-			self.get = function (id) {
+
+            self.get = function (id) {
                 var parameters = [id];
                 return DBA.query("SELECT * FROM book WHERE ID = (?)", parameters)
                         .then(function (result) {
                             return DBA.getById(result);
                         });
             }
-			self.update = function (data) {
-                var parameters = [data.actual_a,data.actual_k,data.actual_foc,data.actual_inf,data.actual_tg,data.actual_e,data.actual_arrival,data.actual_time,data.ID];
-                  return DBA.query("UPDATE book \n\
+            self.update = function (data) {
+                var parameters = [data.actual_a, data.actual_k, data.actual_foc, data.actual_inf, data.actual_tg, data.actual_e, data.actual_arrival, data.actual_time, data.ID];
+                return DBA.query("UPDATE book \n\
 				  SET actual_a = (?),\n\
 				  actual_k = (?),\n\
 				  actual_foc = (?),\n\
@@ -176,7 +180,7 @@ angular.module('app.services', [])
 				  actual_time = (?)\n\
 				 WHERE id = (?)", parameters);
             }
-			
-			
+
+
             return self;
         })
